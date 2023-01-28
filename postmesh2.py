6d271f519a0603b1c7e0a4243669b4d98bb48b5e
Andrew <49123416+gm1ymi@users.noreply.github.com>
2023-01-27 20:31:34 -0500

    Add files via upload

---
+++ postmesh2.py
@@ -0,0 +1,22 @@
+# Converted PHP script from N2MH
+# Reads contents of 'readme.txt' and posts to MeshChat
+# Edit callsign, channel, MeshChat_IP and name of textfile as required
+# K1YMI 27/1/2023
+#
+import requests
+import time
+
+from pathlib import Path
+txt = Path('readme.txt').read_text()
+
+payload = {'action': 'send_message',
+           'message': txt,
+           'call_sign': 'K1YMI',
+           'epoch': int(time.time()),
+           'channel': 'test'}
+
+url = "http://<MeshChat_IP>/cgi-bin/meshchat"
+
+response = requests.post(url, data=payload)
+
+print(response.text)
